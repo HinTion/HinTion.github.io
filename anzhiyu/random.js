d87e61efa1cbe7513189d1bb5807d3c95349bf5a
HinTion <1256202766@qq.com>
2024-10-06 18:58:15 +0800

    Site updated: 2024-10-06 18:58:15

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2024/07/05/hello-world/","2024/10/06/图论/","2024/07/06/数论/","2024/07/05/第一次/","2024/07/10/蓝桥杯/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
